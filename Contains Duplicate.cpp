a71d8546d9cadafea4fe51d80a9315095a92b938
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2022-10-28 01:36:29 +0530

    Create   Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    unordered_map<int,int>m;
+    for(int i = 0; i<nums.size(); i++){
+        if(m.count(nums[i]) > 0){
+            return true;
+        }
+        else
+        {
+            m[nums[i]] = i;
+        }
+            
+    }
+    return false; 
+   }
+};
+
+    
