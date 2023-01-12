22c89e9d10dbf8899b4c5c3b9c20f69b97e09911
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-01-13 02:16:53 +0530

    Create Richest Customer Wealth.cpp

---
+++ Richest Customer Wealth.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int sum=0,temp=0,m=accounts.size();
+        for(int i=0;i<m;i++){
+            temp=accumulate(accounts[i].begin(),accounts[i].end(),0);
+            if(temp>sum)
+                sum=temp;
+        }
+        return sum;
+    }
+};
