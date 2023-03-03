b98d99885cd66c50dd27d98f0db876babb1a5bbf
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-04 01:56:30 +0530

    Create The Lead Game.cpp

---
+++ The Lead Game.cpp
@@ -0,0 +1,33 @@
+//https://www.codechef.com/problems/TLG?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	int s1=0,s2=0,max1=0,max2=0;
+	for(int i=0;i<n;i++){
+	    int x,y;
+	    cin>>x>>y;
+	    s1+=x;
+	    s2+=y;
+	    if(s1>s2){
+	        if(s1-s2>max1){
+	            max1=s1-s2;
+	        }
+	    }
+	    else if(s1<s2){
+	        if(s2-s1>max2){
+	            max2=s2-s1;
+	        }
+	    }
+	}
+	if(max1>max2){
+	    cout<<1<<" "<<max1<<endl;
+	}
+	else{
+	    cout<<2<<" "<<max2<<endl;
+	}
+	return 0;
+}
