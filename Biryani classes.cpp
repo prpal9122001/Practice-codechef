75c28f1f01a984c0336795786ddf5853664aaed5
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 20:48:07 +0530

    Create Biryani classes.cpp

---
+++ Biryani classes.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/BIRYANI?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<x*y<<endl;
+    
+    }
+	// your code goes here
+	return 0;
+}
