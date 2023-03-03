3d77ea73c0b3b28dd9729cfc37aee6ba5d31808c
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 22:49:42 +0530

    Create (Begi) Kitchen Timings.cpp

---
+++ (Begi) Kitchen Timings.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/KITCHENTIME?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<y-x<<endl;
+    }
+	// your code goes here
+	return 0;
+}
