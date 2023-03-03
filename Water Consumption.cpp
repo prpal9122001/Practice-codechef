e438ea76c2dc901b97180b1e31fda0c415b482a3
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 20:35:30 +0530

    Create Water Consumption.cpp

---
+++ Water Consumption.cpp
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/WATERCONS?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>=2000)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+
+}
