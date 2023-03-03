dcde14e3dc7405f5addb7174d82628efa87b7d32
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 23:05:51 +0530

    Create (Begi)Tom and Jerry Chase.cpp

---
+++ (Begi)Tom and Jerry Chase.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/JERRYCHASE?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(y>x){
+        cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
