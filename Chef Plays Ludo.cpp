fae38961c011819b26e6c21225c8140f124494c5
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 21:12:52 +0530

    Create Chef Plays Ludo.cpp

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/LUDO?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<6)
+        {cout<<"NO"<<endl;
+        }else if(x=6){
+            cout<<"YES"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
