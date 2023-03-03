13bd6ad0b03c8dc46b19f65c50c243074e6d5a57
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 23:33:55 +0530

    Create (Begi)Get Subscription.cpp

---
+++ (Begi)Get Subscription.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/SUBSCRIBE?tab=submissions
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>30){
+        cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+        
+    }
+	// your code goes here
+	return 0;
+}
