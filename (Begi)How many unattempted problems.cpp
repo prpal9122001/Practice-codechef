3eb3d6f8561ff5b5b0639c362cde871b81e82e42
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 21:35:23 +0530

    Create (Begi)How many unattempted problems.cpp

---
+++ (Begi)How many unattempted problems.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/PRACLIST?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        int x,y;
+        cin>>x>>y;
+	    cout<<(x-y)<<endl;
+	// your code goes here
+	return 0;
+}
