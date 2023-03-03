de8eb71c58070d4917f0d808ffbb268d640c9672
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 22:40:16 +0530

    Create (Begi)Determine the Score.cpp

---
+++ (Begi)Determine the Score.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/DETSCORE?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,n;
+        cin>>x>>n;
+        int count=((x*n)/10);
+	    cout<<count<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
