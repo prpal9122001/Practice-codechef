6f058161849c7ae91012865178f1f3b22d7fdc43
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 20:15:33 +0530

    Create Squats.cpp

---
+++ Squats.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/SQUATS?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        cout<<x*15<<endl;
+    }
+	// your code goes here
+	return 0;
+}
