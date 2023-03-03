d5c9ee73a3176cf88ac474e3701c8b0b814d6bb5
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 20:24:57 +0530

    Create Saving Taxes.cpp

---
+++ Saving Taxes.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/TAXSAVING?tab=ide
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--);
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<x-y<<endl;
+    }
+    
+	// your code goes here
+	return 0;
+}
