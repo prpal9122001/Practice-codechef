bdb599d4e891da9c79023eab958c8d977891781b
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 19:17:45 +0530

    Create Number Mirror.cpp

---
+++ Number Mirror.cpp
@@ -0,0 +1,12 @@
+//Write a program that takes a number N as the input, and prints it to the output.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout<<N;
+    return 0;
+
+}
