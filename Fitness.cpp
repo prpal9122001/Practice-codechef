25605b5e3075b2a26195aa91992948f14ccc1ab9
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-03 21:05:35 +0530

    Create Fitness.cpp

---
+++ Fitness.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/FIT?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+	    x=x*2*5;
+        cout<<x<<endl;
+    }
+	// your code goes here
+	return 0;
+}
