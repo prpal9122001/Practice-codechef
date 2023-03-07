13aad0829c92919daf731c8615d91482d4af8171
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-07 20:29:50 +0530

    Create (dsa)Buy Please.cpp

---
+++ (dsa)Buy Please.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/CCSTART2/problems/BUYPLSE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y,a,b,n;
+    cin>>x>>y>>a>>b>>n;
+    n=(x*a)+(y*b);
+    cout<<n<<endl;
+	// your code goes here
+	return 0;
+}
