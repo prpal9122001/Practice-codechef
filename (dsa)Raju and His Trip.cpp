e32713305f905850815f087c7151cb07516c09de
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-08 20:24:19 +0530

    Create (dsa)Raju and His Trip.cpp

---
+++ (dsa)Raju and His Trip.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/CCSTART2/problems/VALTRI?tab=submissions
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%5==0 || n%6==0){ //divisible ke liye 
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
