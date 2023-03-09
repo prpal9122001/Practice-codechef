bb79d197f713b66406b9c4c8b4e1345096d48f37
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-10 00:47:34 +0530

    Create (dsa)Triangle With Angle.cpp

---
+++ (dsa)Triangle With Angle.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/CCSTART2/problems/ANGTRICH?tab=submissions
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b+c==180 && a && b && c ){  
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+	// your code goes here
+	return 0;
+}
