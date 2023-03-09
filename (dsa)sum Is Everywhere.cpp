ff0db0c58d211dfbce6c4c43208c28ef9870842a
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-10 00:38:03 +0530

    Create (dsa)sum Is Everywhere.cpp

---
+++ (dsa)sum Is Everywhere.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/CCSTART2/problems/SUMEVOD?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long N,ans;
+	cin>>N;
+    ans=(N*N);
+    cout<<ans<<' '<<ans+N;
+	return 0;
+}
+
