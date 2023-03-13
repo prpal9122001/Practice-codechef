eebab3dccabfad2e4a2b99eb2ee4005b8ce9a53c
prpal9122001 <72788936+prpal9122001@users.noreply.github.com>
2023-03-13 21:21:02 +0530

    Create (ldsa)Symmetric Tree.cpp

---
+++ (ldsa)Symmetric Tree.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/symmetric-tree/submissions/914381546/
+
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        int *arr = new int[n];
+        int maximum = 1;
+        vector<int> flavours(k+1, -1);
+        for(int i=0; i<n; ++i) {
+                cin>>arr[i];
+                maximum = max(maximum, i-flavours[arr[i]]-1);
+                //cout << maximum << " " << i-flavours[arr[i]]-1 << endl;
+                flavours[arr[i]]=i;
+        }
+        
+        for(int i=1; i<=k; i++)
+            maximum = max(maximum, n-flavours[i]-1);
+        
+        cout<<maximum<<'\n';
+        delete [] arr;
+    }
+    return 0;
+}
